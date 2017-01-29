e6c34a889332c913a709d9718328d42cb2e58078
Alena Kuntsevich <ekuntsevich@yandex.ru>
2017-01-29 17:31:21 +0300

    double-linded-list/feature: const, value head and tail, isEmptyI hope are done

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,19 +1,50 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
 
-    append(data) {}
+       this.length = 0;
+    }
 
-    head() {}
+    append(data) {
+        if(this.length ==0) {
 
-    tail() {}
+            this._head = new Node(data);
+            this.length++;
+            this._tail = new Node(data);
+
+
+
+        } else {
+
+                this.cell = new Node(data, this._tail, this._tail.prev.next);
+                this._tail.prev.next = this.cell;
+                this._tail.prev = this.cell;
+            }
+
+
+    }
+
+    head() {
+        return this._head.data;
+
+    }
+
+    tail() {
+        return this._tail.data;
+    }
 
     at(index) {}
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() {
+        if(this.length==0){
+            return true;
+        } else{
+            return false;
+        }
+    }
 
     clear() {}
 
